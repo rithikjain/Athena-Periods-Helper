02f4ebcefdb3baaa5737d31cbac97fddc3d1ab28
Rithik Jain <rithik.jain3006@gmail.com>
2020-03-12 22:19:02 +0530

    Code:Removed a comment

--- app/src/main/java/com/dscvit/periodsapp/ui/requests/RequestsFragment.kt
+++ app/src/main/java/com/dscvit/periodsapp/ui/requests/RequestsFragment.kt
@@ -37,7 +37,6 @@ class RequestsFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_requests, container, false)
     }
 
